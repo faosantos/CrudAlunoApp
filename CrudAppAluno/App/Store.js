a8441dad61d869317c20aa59e8c38c2216b6cd2b
faosantos <faosantos@restinga.ifrs.edu.br>
2019-06-03 14:07:53 -0300

    commit correto

--- CrudAppAluno/App/Store.js
+++ CrudAppAluno/App/Store.js
@@ -3,7 +3,7 @@ import thunk from 'redux-thunk'
 import createLogger from 'redux-logger'
 import { autoRehydrate } from 'redux-persist'
 import createActionBuffer from 'redux-action-buffer'
-import {REHYDRATE} from 'redux-persist/constants'
+import {REHYDRATE} from 'redux-persist/es/constants'
 import reducers from './Reducers'
 
 let middlewares = [thunk, createActionBuffer(REHYDRATE)]
