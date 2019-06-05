db2c0c90447d24cc51bba1616d7d9c63198a3bc3
faosantos <faosantos@restinga.ifrs.edu.br>
2019-06-05 14:53:14 -0300

    commit

--- CrudAppAluno/App/Config/Settings.js
+++ CrudAppAluno/App/Config/Settings.js
@@ -1,3 +1,3 @@
 export const OAUTH_CLIENT_ID = 2
-export const OAUTH_CLIENT_SECRECT = 'D4n8Pln3KqfrJDCZT8rXrkYx37fFldSJjjqUS33n'
+export const OAUTH_CLIENT_SECRECT = 'Hp60LBDv7Gmq809ZcycRPOqtbJaah1dLya8WowRZ'
 
